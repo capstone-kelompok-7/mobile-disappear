1518abba3c083e7c5d22926cb257f35a7a6023aa
iqbalgany <rismagany1203@gmail.com>
2023-11-07 14:02:29 +0700

    feature/forgot_password: 1. mengganti dialog bottom sheet

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -9,124 +9,119 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              /// Title
-              Padding(
-                padding: EdgeInsets.only(
-                  right: 72,
-                  left: 72,
-                  top: 92,
+        backgroundColor: Colors.grey,
+        body: ListView(
+          children: [
+            /// Title
+            Padding(
+              padding: EdgeInsets.only(
+                right: 72,
+                left: 72,
+                top: 92,
+              ),
+              child: Text(
+                'Disappear',
+                style: GoogleFonts.inter().copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 48,
                 ),
-                child: Text(
-                  'Disappear',
-                  style: GoogleFonts.inter().copyWith(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 48,
+              ),
+            ),
+
+            /// Bottom Sheet
+            Container(
+              margin: EdgeInsets.only(
+                top: 150,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(40),
+                ),
+              ),
+              child: Container(
+                width: 390,
+                height: 573,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          right: 47,
+                          left: 47,
+                          top: 40,
+                          bottom: 65,
+                        ),
+                        child: Text(
+                          'Lupa Kata Sandi',
+                          style: GoogleFonts.inter().copyWith(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 26,
+                          ),
+                        ),
+                      ),
+
+                      /// Email
+                      Container(
+                        height: 80,
+                        width: 295,
+                        margin: EdgeInsets.only(
+                          bottom: 40,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Email',
+                              style: GoogleFonts.inter().copyWith(
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                hintText: 'Masukkan email anda',
+                                hintStyle: GoogleFonts.inter().copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      /// Lanjut
+                      Container(
+                        height: 44,
+                        width: 295,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.grey,
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Lanjut',
+                            style: GoogleFonts.inter().copyWith(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              SizedBox(
-                height: 30,
-              ),
-
-              /// Bottom Sheet
-              ElevatedButton(
-                child: Text('Reset Password'),
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(50),
-                      ),
-                    ),
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: 390,
-                        height: 573,
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  right: 47,
-                                  left: 47,
-                                  top: 40,
-                                  bottom: 65,
-                                ),
-                                child: Text(
-                                  'Lupa Kata Sandi',
-                                  style: GoogleFonts.inter().copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 26,
-                                  ),
-                                ),
-                              ),
-
-                              /// Email
-                              Container(
-                                height: 80,
-                                width: 295,
-                                margin: EdgeInsets.only(
-                                  bottom: 40,
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Email',
-                                      style: GoogleFonts.inter().copyWith(
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                    TextField(
-                                      decoration: InputDecoration(
-                                        hintText: 'Masukkan email anda',
-                                        hintStyle: GoogleFonts.inter().copyWith(
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 12,
-                                        ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide:
-                                              BorderSide(color: Colors.black),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-
-                              /// Lanjut
-                              Container(
-                                height: 44,
-                                width: 295,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.grey,
-                                ),
-                                child: TextButton(
-                                  onPressed: () {},
-                                  child: Text(
-                                    'Lanjut',
-                                    style: GoogleFonts.inter().copyWith(
-                                        fontWeight: FontWeight.w700,
-                                        fontSize: 20,
-                                        color: Colors.black),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
