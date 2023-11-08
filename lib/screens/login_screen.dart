8f1ea0d3bb5dc995b3507c0b91d008072e988b72
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2023-11-08 15:58:00 +0700

    feature/splash_screen : menambahkan hlm splash screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routePath = '/';
+
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('login'),
+    );
+  }
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  static const String routePath = '/';
+
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('registes'),
+    );
+  }
+}
