340ebf7c1d89e65826e91bdf570a11e0f87a795c
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-11-15 15:32:24 +0800

    set intialroute to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: SearchProductScreen.routePath,
+      initialRoute: SplashScreen.routePath,
       routes: {
         SplashScreen.routePath: (context) => const SplashScreen(),
         HomeScreen.routePath: (context) => const HomeScreen(),
