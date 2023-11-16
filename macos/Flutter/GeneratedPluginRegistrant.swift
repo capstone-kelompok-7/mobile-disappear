f2f8a0b1f761fec6704dcc22b6e68cbef9e2c0a2
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-11-16 19:29:56 +0800

    remove const

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
