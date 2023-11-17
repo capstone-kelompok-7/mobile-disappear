23184b7257294ee91ba23ae2de9d728e17ca9a15
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-11-17 11:23:25 +0800

    add .env file

---
+++ lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+final class _Env {
+  static const String apiUrl =
+      'https://test.disappear-organization.my.id/api/v1';
+}
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:disappear/env/env.dart';
 
 class AuthService {
   Future<dynamic> login(String email, String password) async {
     final dio = Dio();
 
     Response response = await dio.post(
-      'https://test.disappear-organization.my.id/api/v1/auth/login',
+      '${Env.apiUrl}/auth/login',
       data: {
         'email': email,
         'password': password
