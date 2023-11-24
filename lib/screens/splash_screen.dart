b869409fe5af61d3f2b92f70b7c1518fa83c2337
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-11-24 08:58:33 +0800

    remove unused

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:disappear/screens/auth/login_screen.dart';
-import 'package:disappear/screens/auth/register/register_screen.dart';
 import 'package:disappear/screens/main_screen.dart';
 import 'package:disappear/screens/onboarding/onboarding_screen.dart';
 import 'package:disappear/themes/color_scheme.dart';
