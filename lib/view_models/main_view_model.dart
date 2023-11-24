d6603563643e869539c3c77ffa6c4a2586907a49
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-11-24 14:02:42 +0800

    add order list screen to main screen

--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:disappear/screens/challenge/challenge_screen.dart';
 import 'package:disappear/screens/home/home_screen.dart';
+import 'package:disappear/screens/order_list/order_list_screen.dart';
 import 'package:disappear/screens/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,7 @@ class MainViewModel extends ChangeNotifier {
   final List<Widget> _screens = [
     const HomeScreen(),
     const ChallengeScreen(),
-    const HomeScreen(),
+    const OrderListScreen(),
     const ProfileScreen(),
   ];
 
