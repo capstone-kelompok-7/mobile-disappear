b900bb7fc0538a59383988d3effc8e1a19d517f5
Tri Bayu Priangga <tpriangga6@gmail.com>
2023-11-28 16:37:53 +0700

    Update initial route in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: ChatBotScreen.routePath,
+      initialRoute: SplashScreen.routePath,
       routes: {
         SplashScreen.routePath: (context) => const SplashScreen(),
         OnboardingScreen.routePath: (context) => const OnboardingScreen(),
