6f5a1f91942fc566b36271d6c9fa65982d368939
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-12-12 20:04:00 +0800

    add padding to cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -217,13 +217,16 @@ class _CartScreenState extends State<CartScreen> {
             );
           }
 
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset('assets/img/EmptyCart.svg', width: 200, height: 200,),
-              const SizedBox(height: 40,),
-              const Text('Oops.. Keranjang masih kosong, nih.', style: mediumBody3, textAlign: TextAlign.center,)
-            ],
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: SvgPicture.asset('assets/img/EmptyCart.svg', width: 200, height: 200,)),
+                const SizedBox(height: 40,),
+                const Text('Oops.. Keranjang masih kosong, nih.', style: mediumBody3, textAlign: TextAlign.center,)
+              ],
+            ),
           );
         }
       ),
