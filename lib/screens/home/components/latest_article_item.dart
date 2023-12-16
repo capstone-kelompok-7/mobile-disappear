eed2daaf6fb09a741343fdb9a78f51f47d3a2c7c
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-12-16 12:18:20 +0800

    bookmark article

--- lib/screens/home/components/latest_article_item.dart
+++ lib/screens/home/components/latest_article_item.dart
@@ -1,9 +1,12 @@
+import 'package:disappear/helper.dart';
 import 'package:disappear/models/home/challenge_article.dart';
 import 'package:disappear/screens/article/detail_article_screen.dart';
+import 'package:disappear/screens/article/placeholders/list_article_thumbnail_placeholder.dart';
 import 'package:disappear/screens/home/components/placeholders/latest_article_thumbnail_placeholder.dart';
 import 'package:disappear/themes/color_scheme.dart';
 import 'package:disappear/themes/text_theme.dart';
 import 'package:disappear/view_models/article/Detail_articles_view_model.dart';
+import 'package:disappear/view_models/article/bookmark_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,77 +29,101 @@ class _LatestArticleItemState extends State<LatestArticleItem> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _goToDetailArticleScreen,
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                flex: 1,
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              flex: 1,
+              child: GestureDetector(
+                onTap: _goToDetailArticleScreen,
                 child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: Builder(builder: (context) {
-                      if (widget.article.photo != '') {
-                        return Image.network(
-                          widget.article.photo,
-                          height: 120,
-                          fit: BoxFit.cover,
-                          loadingBuilder: (context, child, loadingProgress) {
-                            if (loadingProgress != null) {
-                              return const LatestArticleThumbnailPlaceholder();
-                            }
-
-                            return child;
-                          },
-                        );
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network(
+                    widget.article.photo,
+                    height: 120,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress != null) {
+                        return const ListArticleThumbnailPlaceholder();
                       }
-
-                      return Image.asset(
-                        'assets/img/tumbler.png',
-                        height: 120,
-                        fit: BoxFit.cover,
-                      );
-                    })),
-              ),
-              Flexible(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(widget.article.formattedDate, maxLines: 2, style: regularBody8.copyWith(color: neutral40), overflow: TextOverflow.ellipsis,),
-                      const SizedBox(height: 10),
-                      Text(widget.article.title, style: semiBoldBody6),
-                      const SizedBox(height: 15),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Row(
-                            children: [
-                              Text(widget.article.createdAgo, style: mediumBody8),
-                              const SizedBox(width: 10),
-                              const Icon(Icons.visibility, size: 18),
-                              const SizedBox(width: 5),
-                              Text(widget.article.views.toString(), style: regularBody8),
-                            ],
-                          ),
-                          const Icon(
-                            Icons.bookmark_outline,
-                            size: 18,
-                          ),
-                        ],
-                      ),
-                    ],
+                      return child;
+                    },
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(widget.article.formattedDate,
+                        style: mediumBody8.copyWith(color: primary40)),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: _goToDetailArticleScreen,
+                      child: Text(widget.article.title, style: semiBoldBody6)
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              formatToTimeago(widget.article.createdAt),
+                              style: regularBody8.copyWith(
+                                color: primary40,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Icon(
+                              Icons.visibility,
+                              size: 18,
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              widget.article.views.toString(),
+                              style: regularBody8.copyWith(
+                                color: primary40,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Consumer<BookmarkViewModel>(
+                          builder: (context, viewModel, _) {
+                            return GestureDetector(
+                              onTap: () => viewModel.toggleBookmark(widget.article.id),
+                              child: Icon(
+                                viewModel.isBookmarked(widget.article.id)
+                                    ? Icons.bookmark
+                                    : Icons.bookmark_outline,
+                                size: 18,
+                              ),
+                            );
+                          }
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
